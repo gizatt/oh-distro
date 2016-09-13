8ecfb30b265658127d9d3f891d86958f0985afbf
Greg Izatt <gizatt@mit.edu>
2016-09-13 13:52:18 -0400

    Nonpenetration and tinybolt tracking

--- software/models/IRB140/urdf/gen.sh
+++ software/models/IRB140/urdf/gen.sh
@@ -1,1 +1,2 @@
+rosrun xacro xacro.py -o irb_140_ati_schunk_gelsight.urdf irb_140_ati_schunk_gelsight.urdf.xacro
 rosrun xacro xacro.py -o irb_140_ati_schunk_gelsight_less_collision.urdf irb_140_ati_schunk_gelsight_less_collision.urdf.xacro
